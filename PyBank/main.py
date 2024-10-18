8ad263aa5549aa2e9be9db6ee87bd181a2210d3c
DanielECab <dec2194@gmail.com>
2024-10-17 17:29:44 -0700

    Updated files to include annotation

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,21 @@
+#Importing csv and os, loding the file using path.join.
+
 import csv
 import os
 
 file_to_load = os.path.join("Resources", "budget_data.csv")
 file_to_output = os.path.join("analysis", "budget_analysis.txt")
-                              
+
+  #Setting the variables for the Pybank.
+
 total_months = 0
 total_net = 0
 monthly_change =[]
 net_change_list = []
 greatest_increase = ["", 0]
 greatest_decrease = ["", 999999999999999999]
+
+#Opening and reading the csv file
 
 with open(file_to_load) as financial_data:
     reader = csv.reader(financial_data)
@@ -19,6 +25,9 @@ with open(file_to_load) as financial_data:
     total_months = total_months + 1
     total_net = total_net + int(first_row[1])
     Prev_net = int(first_row[1])
+   
+   #Loop to calculate the total and net change
+
     for row in reader:
         total_months = total_months + 1
         total_net = total_net + int(row[1])
@@ -36,7 +45,11 @@ with open(file_to_load) as financial_data:
             greatest_decrease[0] = row[0]
             greatest_decrease[1] = net_change
 
+#Calculating the average
+
 monthly_avg = sum(net_change_list) / len(net_change_list)
+
+#Printing the output to txt file.
 
 output = (
     f"\nFinancial Analysis\n"
@@ -48,6 +61,8 @@ output = (
     f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
 print(output)
+
+#Results as a txt file
 
 with open(file_to_output, "w") as txt_file:
     txt_file.write(output)
--- PyPoll/py_poll_main.py
+++ PyPoll/py_poll_main.py
@@ -1,8 +1,12 @@
+#Importing the csv and os and losaing the files using path.join.
+
 import csv
 import os
 
 file_to_load = os.path.join("Resources", "election_data.csv")
 file_to_output = os.path.join("analysis", "election_analysis.txt")
+
+#Setting up the variables to track the poll results.
 
 total_votes = 0
 candidates = []
@@ -10,9 +14,14 @@ candidate_votes = {}
 winners_count = 0
 winner = ""
 
+#Setting up the reader for the csv and skipping the header row.
+
 with open(file_to_load) as election_data:
     reader = csv.reader(election_data)
     header = next(reader)
+   
+    #Loop for the dataset.
+
     for row in reader:
         total_votes += 1
         candidate = row[2]
@@ -22,6 +31,9 @@ with open(file_to_load) as election_data:
 
         candidate_votes[candidate] = candidate_votes[candidate] +1
 
+
+#Printing the output for the txt file.
+
 with open(file_to_output, "w") as txt_file:
     election_header = (
         f"Election Results \n"
@@ -30,6 +42,8 @@ with open(file_to_output, "w") as txt_file:
         f"------------------ \n"
     )
     txt_file.write(election_header)
+
+#Loop for finding which candidate wins.
 
     for candidate in candidate_votes:
         votes = candidate_votes[candidate]
@@ -41,6 +55,7 @@ with open(file_to_output, "w") as txt_file:
         print(voter_output)
         txt_file.write(voter_output)
 
+#Winner of the election.
     winner_output = (
         f"--------------\n"
         f"Winner: {winner} \n"
